4db483bc94ddfdbad0d4d1e2bd738d6bbd151085
uliadib12 <uliadib@gmail.com>
2023-06-15 16:26:10 +0700

    fix url

--- lib/utils/url.dart
+++ lib/utils/url.dart
@@ -1,4 +1,4 @@
 class URL {
-  // static const String BASE_URL = "https://api-event-kampus.azurewebsites.net/";
-  static const String BASE_URL = "http://localhost:8080/";
+  static const String BASE_URL = "https://api-event-kampus.azurewebsites.net/";
+  // static const String BASE_URL = "http://localhost:8080/";
 }
