9be086133d31e7e0a58f0fd20260f64a2e3f962e
uliadib12 <uliadib@gmail.com>
2023-06-19 18:15:36 +0700

    add peserta

---
+++ lib/controller/peserta_controller.dart
@@ -0,0 +1,51 @@
+import 'package:get/get.dart';
+import 'package:tekno_expo/controller/user_controller.dart';
+import 'package:http/http.dart' as http;
+import 'package:tekno_expo/utils/url.dart';
+import 'dart:convert';
+import 'package:tekno_expo/utils/token.dart';
+
+class PesertaController extends GetxController {
+  RxBool isLoading = true.obs;
+  RxBool isError = false.obs;
+  RxString errorMassage = ''.obs;
+
+  RxList _listPeserta = [].obs;
+
+  bool get getIsLoading =>
+      isLoading.value || Get.find<UserController>().isLoading.value;
+
+  List get getListPeserta => _listPeserta;
+
+  @override
+  void onInit() {
+    super.onInit();
+    getPeserta();
+  }
+
+  Future<void> getPeserta() async {
+    try {
+      // get Token
+      String? token = await Token.getToken();
+      if (token == null) {
+        token = '';
+      }
+
+      isLoading.value = true;
+      final result = await http.get(Uri.parse('${URL.BASE_URL}peserta'),
+          headers: {'Authorization': 'Bearer ${token}}'});
+      if (result.statusCode == 200) {
+        final data = jsonDecode(result.body);
+        _listPeserta.value = data;
+        print(_listPeserta);
+      } else {
+        throw Exception('Gagal mengambil data');
+      }
+    } catch (e) {
+      isError.value = true;
+      errorMassage.value = e.toString();
+    } finally {
+      isLoading.value = false;
+    }
+  }
+}
--- lib/menu/peserta.dart
+++ lib/menu/peserta.dart
@@ -1,32 +1,47 @@
 import 'package:flutter/material.dart';
+import 'package:get/get.dart';
+import 'package:tekno_expo/controller/peserta_controller.dart';
 
-class PesertaPage extends StatelessWidget {
+class PesertaPage extends StatefulWidget {
+  @override
+  State<PesertaPage> createState() => _PesertaPageState();
+}
+
+class _PesertaPageState extends State<PesertaPage> {
+  PesertaController pesertaController = Get.put(PesertaController());
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(
-        backgroundColor: Color(0xFF001431),
-        title: Text('Daftar Peserta'),
-      ),
-      body: ListView.builder(
-        itemCount: 5,
-        itemBuilder: (context, index) {
-          return ListTile(
-            leading: CircleAvatar(
-              backgroundColor: Color(0xFF001431),
-              child: Text('${index + 1}'),
-            ),
-            title: Text(
-              'Peserta ${index + 1}',
-              style: TextStyle(fontSize: 18, fontWeight: FontWeight.bold),
-            ),
-            subtitle: Text(
-              'Email: peserta${index + 1}@example.com',
-              style: TextStyle(fontSize: 16, fontWeight: FontWeight.bold),
-            ),
+        appBar: AppBar(
+          backgroundColor: Color(0xFF001431),
+          title: Text('Daftar Peserta'),
+        ),
+        body: Obx(() {
+          if (pesertaController.getIsLoading) {
+            return Center(
+              child: CircularProgressIndicator(),
+            );
+          }
+          return ListView.builder(
+            itemCount: pesertaController.getListPeserta.length,
+            itemBuilder: (context, index) {
+              return ListTile(
+                leading: CircleAvatar(
+                  backgroundColor: Color(0xFF001431),
+                  child: Text('${index + 1}'),
+                ),
+                title: Text(
+                  '${pesertaController.getListPeserta[index]['username']}',
+                  style: TextStyle(fontSize: 18, fontWeight: FontWeight.bold),
+                ),
+                subtitle: Text(
+                  'Email: ${pesertaController.getListPeserta[index]['email']}',
+                  style: TextStyle(fontSize: 16, fontWeight: FontWeight.bold),
+                ),
+              );
+            },
           );
-        },
-      ),
-    );
+        }));
   }
 }
